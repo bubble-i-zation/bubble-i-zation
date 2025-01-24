4edbb8a9da1627f3ff0f448bd0d936af578d3639
alexander <43758870+alexw-at@users.noreply.github.com>
2025-01-24 21:44:15 +0100

    add porter sprites

--- bubble-i-zation/scenes/porter/porter.gd
+++ bubble-i-zation/scenes/porter/porter.gd
@@ -5,18 +5,22 @@ const speed := 30
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
 @export var bubbles: Array[Marker2D] = []
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 var current_target: Marker2D = null
 
 func _ready() -> void:
 	current_target = bubbles.pick_random()
+	animated_sprite_2d.play("walk")
 
 
 func _physics_process(_delta: float) -> void:
 	if navigation_agent_2d.is_navigation_finished():
 		print("target reached")
 		current_target = bubbles.pick_random()
+	
+	animated_sprite_2d.flip_h = velocity.x < 0
 
 	navigation_agent_2d.target_position = current_target.global_position
 	var next_pos := navigation_agent_2d.get_next_path_position()
