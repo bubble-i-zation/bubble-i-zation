f6f87235978d0e56a0e800f9309738617ef27954
alexander <43758870+alexw-at@users.noreply.github.com>
2025-01-24 21:13:09 +0100

    add navigation

---
+++ bubble-i-zation/scenes/porter/porter.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const speed := 20
+
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+
+@export var bubbles: Array[Marker2D] = []
+
+
+var current_target: Marker2D = null
+
+func _ready() -> void:
+	current_target = bubbles.pick_random()
+
+
+func _physics_process(_delta: float) -> void:
+	if navigation_agent_2d.is_navigation_finished():
+		print("target reached")
+		current_target = bubbles.pick_random()
+
+	navigation_agent_2d.target_position = current_target.global_position
+	var next_pos := navigation_agent_2d.get_next_path_position()
+	var new_velocity := global_position.direction_to(next_pos) * speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity_forced(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
+	
+	move_and_slide()
+		
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
