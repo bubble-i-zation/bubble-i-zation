ef83b31ab55aa5f64c3bd7ee142d186e629bd0cc
alexander <43758870+alexw-at@users.noreply.github.com>
2025-01-25 14:46:11 +0100

    allow multiple zoom levels

--- bubble-i-zation/scenes/cameraMovement.gd
+++ bubble-i-zation/scenes/cameraMovement.gd
@@ -1,48 +1,32 @@
 extends Camera2D
 
-var maxSpeed = 400
-var speed = 1
-var acceleration = 24
-var move_direction = Vector2(0,0)
-var motion
-#zoom
-var currentZoom
-var newZoom
-var zoomSpeed = 10
-var zoomMargin = 0.1
-var zoomMin = 1
-var zoomMax = 3
-var zoomfactor = 1
+const speed := 10
 
+var currentZoom: float
+var zoomMin := 1
+var zoomMax := 5
+var zoom_target: float
 
 func _ready():
-	currentZoom = zoom
+	currentZoom = zoom.x
+	zoom_target = zoom.x
+
 func _process(delta: float) -> void:
-	movementLoop(delta)
-	position += motion
-	#zoom
+	move_camera()
+
+	if Input.is_action_just_pressed("Zoom-In") && zoom_target < zoomMax:
+		zoom_target += 0.5
+	if Input.is_action_just_pressed("Zoom-Out") && zoom_target > zoomMin:
+		zoom_target -= 0.5
 	
-	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomSpeed * delta)
-	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomSpeed * delta)
-	
-	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
-	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
-	
-func _input(event):
-	if (Input.is_action_pressed("Zoom-Out")):
-		zoomfactor -= 0.1
-		if zoomfactor < 0:
-			zoomfactor = 0
-	if (Input.is_action_pressed("Zoom-In")):
-		zoomfactor += 0.1
-		if zoomfactor > 1:
-			zoomfactor = 1
-	
-func movementLoop(delta):
+	currentZoom = lerpf(currentZoom, zoom_target, 8 * delta)
+	zoom = Vector2(currentZoom, currentZoom)
+
+func move_camera():
+	var move_direction := Vector2(0,0)
 	move_direction.x = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
 	move_direction.y = int(Input.is_action_pressed("Down")) - int(Input.is_action_pressed("Up")) / float(2)
-	if move_direction != Vector2(0,0):
-		speed += acceleration * delta
-	else:
-		speed = 1
-	motion = move_direction.normalized() * speed
+
+	var motion = move_direction.normalized() * speed * (1 / currentZoom)
+	
+	position += motion
