36b9cbdf2f257bd3c5c06f00aa7eaf89cf3fda70
alexander <43758870+alexw-at@users.noreply.github.com>
2025-01-26 11:19:00 +0100

    fix transport job bounce while item not found

--- bubble-i-zation/scenes/porter/porter.gd
+++ bubble-i-zation/scenes/porter/porter.gd
@@ -74,6 +74,10 @@ func do_navigation():
 	if !navigation_agent_2d.is_target_reachable():
 		velocity = Vector2.ZERO
 		return
+	
+	if current_job is TransportJob && !current_job.found_item:
+		velocity = Vector2.ZERO
+		return
 
 	var next_pos := navigation_agent_2d.get_next_path_position()
 	var new_velocity := global_position.direction_to(next_pos) * speed
